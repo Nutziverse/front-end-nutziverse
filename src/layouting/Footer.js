9609d5699d901a2a4c361c23a8e9c7fc4761da6c
Muhammad Nurudin <muhammadnurudin01@gmail.com>
2022-01-02 12:37:15 +0700

    add header footer

--- src/layouting/Footer.js
+++ src/layouting/Footer.js
@@ -1,38 +1,43 @@
 import "../style/footer.css";
-import logo_nutziverse from "../images/logo_nutziverse.png";
+import logo_nutziverse from "../images/footer_nutzyverse.png";
 
 export default function Footer() {
   return (
-    <div className="box">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <div>
-              <img className="me-auto mb-3" src={logo_nutziverse} height="50px" alt="Logo"></img>
-              <p>Nutrisi and Gizi for our universe</p>
-              <p>Jl. Jenderal Sudirman, Senayan, Jakarta 10270</p>
+      <div class="mt-5 box">
+        <div className="container">
+          <div class="row">
+            <div class="col-4">
+              <div>
+                <img className="me-auto mb-3" src={logo_nutziverse} height="50px" alt="Logo"></img>
+                <p>Nutrisi and Gizi for our universe</p>
+                <p>Jl. Jenderal Sudirman, Senayan, Jakarta 10270</p>
+              </div>
+            </div>
+            <div class="col-4">
+              <div className="d-flex">
+                <div className="ms-auto me-auto">
+                  <p>Tentang Kami</p>
+                  <p>Kebijakan Privasi</p>
+                  <p>Hubungi Kami</p>
+                </div>
+              </div>
+              
+              
+            </div>
+            <div class="col-4">
+              <p>Partner Kami</p>
+              <img src="https://logos-world.net/wp-content/uploads/2020/03/Danone-logo.png" height="80px" width="160px" alt="Logo"></img>
+              <img src="https://blud.co.id/wp/wp-content/uploads/2018/02/logo-kemenkes.png" height="80px" width="160px" alt="Logo"></img>
             </div>
           </div>
-          <div class="two col">
-            <p>Tentang Kami</p>
-            <p>Kebijakan Privasi</p>
-            <p>Hubungi Kami</p>
-          </div>
-          <div class="col">
-            <p>Partner Kami</p>
-            <img src="https://logos-world.net/wp-content/uploads/2020/03/Danone-logo.png" height="80px" width="160px" alt="Logo"></img>
-            <img src="https://blud.co.id/wp/wp-content/uploads/2018/02/logo-kemenkes.png" height="80px" width="160px" alt="Logo"></img>
+          <div class="copyright">
+            <div class="row mb-4">
+              <div class="col-lg-4"></div>
+              <div class="col-lg-4">Copyright © Danone 2021 | All Right Reserved</div>
+              <div class="col-lg-4"></div>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div class="copyright">
-        <div class="row justify-content-md-center">
-          <div class="col col-lg-2"></div>
-          <div class="col-md-auto">Copyright © Danone 2021 | All Right Reserved</div>
-          <div class="col col-lg-2"></div>
-        </div>
-      </div>
     </div>
   );
 }

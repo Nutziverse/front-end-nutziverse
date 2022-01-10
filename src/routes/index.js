5ec377cfb3f26c3b8cd27b48f6d1156d987f1cf6
Jessica Wong <wongjessica124@gmail.com>
2022-01-11 03:03:18 +0700

    Initial pilih makanan commit without search

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import SignIn from "../pages/SignIn";
 import Akun from "../pages/Akun";
 import SignUp from "../pages/SignUp";
 import AkunGoogle from "../pages/AkunGoogle";
+import PilihMakanan from "../pages/PilihMakanan";
 
 
 export default function Routers() {
@@ -18,7 +19,7 @@ export default function Routers() {
       <Route path="/sign-up" element={<SignUp />}></Route>
       <Route path="/akun" element={<Akun />}></Route>
       <Route path="/akungoogle" element={<AkunGoogle />}></Route>
-      
+      <Route path="/pilih-makanan" element={<PilihMakanan />}></Route>
       
   
     </Routes>

5792f9d9721fd9ba86c50c3790151d2134f45626
Jessica Wong <wongjessica124@gmail.com>
2022-01-14 10:19:26 +0700

    update profile unfinished

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,13 +1,25 @@
 
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Navigate } from "react-router-dom";
 import { getCookie } from "../helpers";
 import Layout from "../layouting/Layout";
+import { getUSER } from "../redux/actions/action.user";
 import "../style/card-makanan.css"
 
 export default function Profile() {
-  const token = getCookie("token")
-  if(!token) {
-    return <Navigate to="/" />
+  let token = getCookie("token")
+  const dispatch = useDispatch()
+  const profile = useSelector(state => state.userReducer)
+  const {User, loading, error} = profile
+
+  useEffect(() => {
+    dispatch(getUSER())
+  }, [dispatch])
+
+  if(!token || error) {
+    return <Navigate to="/unauhorized" />
   }
 
   return(
@@ -15,46 +27,50 @@ export default function Profile() {
       <section className="container py-5">
         <div className="card border-0 shadow-sm">
           <div class="card-body">
-            <div className="row">
-              <div className="col-2">
-                <h1 className="mb-0 text-primary profile-icon">
-                  <i className="far fa-user-circle"></i>
-                </h1>
-              </div>
-              <div className="col-9">
+            {
+              !loading && !error ? (
                 <div className="row">
-                  <div className="col-12">
-                    <h2>Vania</h2>
-                    <p className="text-muted fw-semi-bold"><span className="me-3">25tahun</span> <span>Perempuan</span></p>
+                  <div className="col-2">
+                    <h1 className="mb-0 text-primary profile-icon">
+                      <i className="far fa-user-circle"></i>
+                    </h1>
                   </div>
-                  <div className="col-12">
+                  <div className="col-9">
                     <div className="row">
-                      <div className="col">
-                        <div className="card border-0 shadow-sm">
-                          <div className="card-body">
-                            <h6>65kg</h6>
-                          </div>
-                        </div>
+                      <div className="col-12">
+                        <h2>{User.nama}</h2>
+                        <p className="text-muted fw-semi-bold"><span className="me-3">{User.umur}tahun</span> <span>{User.jeniskelamin}</span></p>
                       </div>
-                      <div className="col">
-                        <div className="card border-0 shadow-sm">
-                          <div className="card-body">
-                            <h6>65kg</h6>
+                      <div className="col-12">
+                        <div className="row">
+                          <div className="col">
+                            <div className="card border-0 shadow-sm">
+                              <div className="card-body">
+                                <h6>{User.berat}kg</h6>
+                              </div>
+                            </div>
                           </div>
-                        </div>
-                      </div>
-                      <div className="col">
-                        <div className="card border-0 shadow-sm">
-                          <div className="card-body">
-                            <h6>65kg</h6>
+                          <div className="col">
+                            <div className="card border-0 shadow-sm">
+                              <div className="card-body">
+                                <h6>{User.tinggi}kg</h6>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col">
+                            <div className="card border-0 shadow-sm">
+                              <div className="card-body">
+                                <h6>{User.aktivitasFisik.keterangan}</h6>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
+              ) : (<h1>Data Profile tidak ada</h1>)
+            }
           </div>
         </div>
       </section>
